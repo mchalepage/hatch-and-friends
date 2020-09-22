53c9b49705e91895e25750439d313273c514f330
mchalepage <me@mchale.page>
2020-09-22 14:05:26 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Who do you think you are? I am.
         </p>
         <a
           className="App-link"
