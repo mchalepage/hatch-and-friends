292728f464859dffd7259520ea7aa06f5187adbe
mchalepage <lemchalepage@gmail.com>
2020-09-23 16:45:30 -0600

    added reset.css and formatted email link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import './reset.css';
 import './App.css';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          We make pictures, products, stories, shorts, and much more. For a portfolio of work, please email katie@hatchandfriends.com.
+          We make pictures, products, stories, shorts, and much more. For a portfolio of work, please email <a className='email-link' href = "mailto: katie@hatchandfriends.com">katie@hatchandfriends.com</a>.
         </p>
       </header>
     </div>
