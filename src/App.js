7d5acbfe0fe8b798614964a8639b826a4f8b68ad
mchalepage <me@mchale.page>
2020-09-24 17:12:26 -0600

    made and added white logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import whiteLogo from './whiteLogo.svg';
 import './reset.css';
 import './App.css';
 
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={whiteLogo} className="App-logo" alt="logo" />
         <p>
           We make pictures, products, stories, shorts, and much more. For a portfolio of work, please email <a className='email-link' href = "mailto: katie@hatchandfriends.com">katie@hatchandfriends.com</a>.
         </p>
