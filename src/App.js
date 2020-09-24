d32603a0ecd1fceb74694e8b59d0a9f3bf2fb039
mchalepage <me@mchale.page>
2020-09-24 17:27:59 -0600

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -6,12 +6,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <img src={whiteLogo} className="App-logo" alt="logo" />
         <p className='description-text'>
           We make pictures, products, stories, shorts, and much more. For a portfolio of work, please email <a className='email-link' href = "mailto: katie@hatchandfriends.com">katie@hatchandfriends.com</a>.
         </p>
-      </header>
     </div>
   );
 }
