21e9e64cfad9703c75cf93a6c44e70c2c1f096a3
mchalepage <me@mchale.page>
2020-10-05 02:54:58 -0600

    created new svg for logo and completed media queries for all breakpoints and screen sizes. site development is complete pending review. icon used in browser tabs will likely need to be changed to something readable or recognizeable.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <img src={whiteLogo} className="App-logo" alt="logo" />
-        <p className='description-text'>
-          We make pictures, products, stories, shorts, and much more. For a portfolio of work, please email <a className='email-link' href = "mailto: katie@hatchandfriends.com">katie@hatchandfriends.com</a>.
-        </p>
+      <img src={whiteLogo} className="App-logo" alt="logo" />
+      <p className='description-text'>
+        We make pictures, products, stories, shorts, and much more. For a portfolio of work, please email <a className='email-link' href = "mailto: katie@hatchandfriends.com">katie@hatchandfriends.com</a>.
+      </p>
     </div>
   );
 }
